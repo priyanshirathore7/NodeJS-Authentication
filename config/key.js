75c8b4d2cfcbfdd384b2dd8c9b1c6495db6f70cf
Priyanshi Rathore <priyanshirathore0720@gmail.com>
2023-06-01 22:19:37 +0530

    Add files via upload

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MongoURI:
+    " mongodb+srv://sagararor403:eFYK6VFtCkR3pEsP@cluster0.7v2xjwd.mongodb.net/?retryWrites=true&w=majority",
+  //   MongoURI: "mongodb://0.0.0.0:27017/backend",
+};
